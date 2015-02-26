8be9a7ecd1da55e6d86584fd5021cc8032d9c7d8
chedinger <edinger.ch@gmail.com>
2015-02-26 20:42:53 +0100

    Refactored game layer

--- chedSpaceInvaders.Shared/SpaceInvadersGameLayer.cs
+++ chedSpaceInvaders.Shared/SpaceInvadersGameLayer.cs
@@ -5,34 +5,39 @@ namespace chedSpaceInvaders.Shared
 {
 	public class SpaceInvadersGameLayer : CCLayerColor
 	{
-		private CCSprite background1;
-		private CCSprite background2;
+		private const string SCROLLING_BG = "scrollingBG";
+		private CCSprite bgPart1;
+		private CCSprite bgPart2;
 
 		public SpaceInvadersGameLayer ()
 		{
-			background1 = new CCSprite ("scrollingBG");
-			background1.AnchorPoint = new CCPoint (0, 0);
-			background1.Position = new CCPoint (0, 0);
-			AddChild (background1);
+			InitializeScrollingBackground ();
+		}
 
-			background2 = new CCSprite ("scrollingBG");
-			background2.AnchorPoint = new CCPoint (0, 0);
-			background2.Position = new CCPoint (0, background1.BoundingBox.Size.Height);
-			AddChild (background2);
+		private void InitializeScrollingBackground ()
+		{
+			bgPart1 = new CCSprite (SCROLLING_BG);
+			bgPart1.AnchorPoint = new CCPoint (0, 0);
+			bgPart1.Position = new CCPoint (0, 0);
+			AddChild (bgPart1);
 
-			Schedule(t => Scroll(new CCDelayTime(0.01f)));
+			bgPart2 = new CCSprite (SCROLLING_BG);
+			bgPart2.AnchorPoint = new CCPoint (0, 0);
+			bgPart2.Position = new CCPoint (0, bgPart1.BoundingBox.Size.Height);
+			AddChild (bgPart2);
+			Schedule (t => Scroll (new CCDelayTime (0.01f)));
 		}
 
 		private void Scroll(CCDelayTime dt) 
 		{
-			background1.Position = new CCPoint (background1.Position.X, background1.Position.Y - 1);
-			background2.Position = new CCPoint (background2.Position.X, background2.Position.Y - 1);
+			bgPart1.Position = new CCPoint (bgPart1.Position.X, bgPart1.Position.Y - 1);
+			bgPart2.Position = new CCPoint (bgPart2.Position.X, bgPart2.Position.Y - 1);
 
-			if (background1.Position.Y < -background1.BoundingBox.Size.Height) 
-				background1.Position = new CCPoint (background2.Position.X, background1.Position.Y + (background2.BoundingBox.Size.Height * 2));
+			if (bgPart1.Position.Y < (-bgPart1.BoundingBox.Size.Height)) 
+				bgPart1.Position = new CCPoint (bgPart2.Position.X, bgPart1.Position.Y + (bgPart2.BoundingBox.Size.Height * 2));
 
-			if (background2.Position.Y < -background2.BoundingBox.Size.Height) 
-				background2.Position = new CCPoint (background1.Position.X, background2.Position.Y + (background1.BoundingBox.Size.Height * 2));
+			if (bgPart2.Position.Y < (-bgPart2.BoundingBox.Size.Height)) 
+				bgPart2.Position = new CCPoint (bgPart1.Position.X, bgPart2.Position.Y + (bgPart1.BoundingBox.Size.Height * 2));
 		}
 
 		protected override void AddedToScene ()
