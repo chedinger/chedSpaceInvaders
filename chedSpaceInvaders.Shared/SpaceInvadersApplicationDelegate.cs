5fd7094b77eefa2d4590e8899c2f2038dedc8bcc
chedinger <edinger.ch@gmail.com>
2015-02-26 11:16:14 +0100

    Initial commit

---
+++ chedSpaceInvaders.Shared/SpaceInvadersApplicationDelegate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CocosSharp;
+using CocosDenshion;
+
+namespace chedSpaceInvaders.Shared
+{
+	public class SpaceInvadersApplicationDelegate : CCApplicationDelegate
+	{
+		public override void ApplicationDidFinishLaunching (CCApplication application, CCWindow mainWindow)
+		{
+			application.PreferMultiSampling = false;
+			application.ContentRootDirectory = "Content";
+
+			mainWindow.SupportedDisplayOrientations = CCDisplayOrientation.Portrait;
+
+			application.ContentSearchPaths.Add ("hd");
+
+			LoadSoundEffects ();
+			LoadStartScene ();
+		}
+
+		private void LoadSoundEffects ()
+		{
+			CCSimpleAudioEngine.SharedEngine.PreloadEffect ("sounds/startup");
+			CCSimpleAudioEngine.SharedEngine.PreloadEffect ("startup");
+		}
+
+		private void LoadStartScene ()
+		{
+
+		}
+
+		public override void ApplicationDidEnterBackground (CCApplication application)
+		{
+			application.Paused = true;
+			CCSimpleAudioEngine.SharedEngine.PauseBackgroundMusic ();
+		}
+
+		public override void ApplicationWillEnterForeground (CCApplication application)
+		{
+			application.Paused = false;
+			CCSimpleAudioEngine.SharedEngine.ResumeBackgroundMusic ();
+		}
+	}
+}
+
---
+++ chedSpaceInvaders.iOS/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+using UIKit;
+using CocosSharp;
+using chedSpaceInvaders.Shared;
+
+namespace chedSpaceInvaders.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		public override void FinishedLaunching (UIApplication application)
+		{
+			var gameApp = new CCApplication ();
+			gameApp.ApplicationDelegate = new SpaceInvadersApplicationDelegate ();
+			gameApp.StartGame ();
+		}
+	}
+}
+
