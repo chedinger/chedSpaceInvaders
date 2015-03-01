63bfdb7a9d5321030e518b77b4605a1c7026989e
chedinger <edinger.ch@gmail.com>
2015-03-01 01:30:23 +0100

    Added ufo and ufo animation to start layer

--- chedSpaceInvaders.Shared/SpaceInvadersStartLayer.cs
+++ chedSpaceInvaders.Shared/SpaceInvadersStartLayer.cs
@@ -7,6 +7,7 @@ namespace chedSpaceInvaders.Shared
 	public class SpaceInvadersStartLayer : CCLayerColor
 	{
 		private CCSprite space;
+		private CCSprite ufo;
 
 		public SpaceInvadersStartLayer() 
 			: base()
@@ -15,8 +16,10 @@ namespace chedSpaceInvaders.Shared
 
 			Color = CCColor3B.Black;
 
-			space = new CCSprite("startupBG");
+			space = new CCSprite ("startupBG");
 			AddChild (space);
+
+			AddUfo ();
 
 			CCSimpleAudioEngine.SharedEngine.PlayEffect("sounds/startup");
 		}
@@ -49,6 +52,22 @@ namespace chedSpaceInvaders.Shared
 				HorizontalAlignment = CCTextAlignment.Center,
 				VerticalAlignment = CCVerticalTextAlignment.Center,
 				AnchorPoint = CCPoint.AnchorMiddle
+			});
+		}
+
+		private void AddUfo ()
+		{
+			ufo = new CCSprite ("ufo");
+			ufo.Position = new CCPoint (-100, -100);
+
+			AddChild (ufo);
+
+			ufo.RunActions (new [] 
+			{
+				new CCMoveTo (1.5f, new CCPoint (100, 200)),
+				new CCMoveTo (1.5f, new CCPoint (500, 500)),
+				new CCMoveTo (1.5f, new CCPoint (200, 950)),
+				new CCMoveTo (1.5f, new CCPoint (360, 800))
 			});
 		}
 
