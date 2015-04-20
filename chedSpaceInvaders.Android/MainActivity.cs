140d0d87e8d93920a3a996554c57ec9fc56d1fca
chedinger <edinger.ch@gmail.com>
2015-04-20 21:17:28 +0200

    Added android project

---
+++ chedSpaceInvaders.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Microsoft.Xna.Framework;
+using CocosSharp;
+using chedSpaceInvaders.Shared;
+
+namespace chedSpaceInvaders.Android
+{
+	[Activity (Label = "chedSpaceInvaders.Android", 
+		AlwaysRetainTaskState = true,
+		Icon = "@drawable/icon",
+		Theme = "@android:style/Theme.NoTitleBar",
+		LaunchMode = LaunchMode.SingleInstance,
+		ScreenOrientation = ScreenOrientation.Portrait,
+		MainLauncher = true,
+		ConfigurationChanges =  ConfigChanges.Keyboard | 
+		ConfigChanges.KeyboardHidden)]
+	public class MainActivity : AndroidGameActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			var gameApp = new CCApplication ();
+
+			gameApp.ApplicationDelegate = new SpaceInvadersApplicationDelegate ();
+			SetContentView (gameApp.AndroidContentView);
+			gameApp.StartGame ();
+		}
+	}
+}
+
+
